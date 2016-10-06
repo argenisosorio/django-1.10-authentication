e859d89d14a79ee3c2e71ee4970cdddb2fa503d7
aosorio <aosorio@cenditel.gob.ve>
2016-10-06 12:42:39 -0400

    Añadidas funciones para la autenticacion de usuarios

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls import url
 from app import views
 from app.views import *
 import app.views as views
-from .views import AppTemplate
+from django.contrib.auth.decorators import login_required
 
 
 
 urlpatterns = [
-    url(r'^$', AppTemplate.as_view(), name='app'),
+	url(r'^$', views.Login.as_view(), name='login'),
+	url(r'success/', login_required(views.Success.as_view(), login_url='login'), name='success'),
+    url(r'logout/$', views.Logout.as_view(), name='logout'),
 ]
--- app/views.py
+++ app/views.py
@@ -11,8 +11,41 @@ from django.views.generic import ListView
 from django.core.urlresolvers import reverse_lazy
 from django.core.urlresolvers import reverse
 from .models import *
+from django.shortcuts import render, redirect
+from django.contrib.auth import forms, login, logout, authenticate
+from django.views.generic import View
 
 
 
-class AppTemplate(TemplateView):
-	template_name = "app/index.html"
+class Login(View):
+  form = forms.AuthenticationForm
+  def get(self, request):
+    context = {'form' : self.form()}
+    return render(request, 'app/login.html', context)
+  def post(self, request):
+    form = self.form(None, request.POST)
+    context = {'form': form}
+    if form.is_valid():
+      username = form.cleaned_data['username']
+      password = form.cleaned_data['password']
+      user = authenticate(username=username, password=password)
+      if user is not None:
+        login(request, user)
+        return redirect('/app/success')
+      else:
+        return render(request, 'app/login.html', context)
+    else:
+      return render(request, 'app/login.html', context)
+
+
+
+class Success(View):
+  def get(self, request):
+    return render(request, 'app/success.html')
+
+
+
+class Logout(View):
+  def get(self, request):
+    logout(request)
+    return redirect('login')
